8118f8c75b7cc33b2795616642abc684c7774e0c
Mariel Reyes Salazar <chiclez@gmail.com>
2021-04-14 02:02:17 +0100

    dimensionality reduction

--- get_recipe.py
+++ get_recipe.py
@@ -41,7 +41,90 @@ def get_data():
 
     return recipes_df, cuisines_df
 
+def top_ingredients(n, df):
+    '''
+    This function will return the Top n ingredients per cuisine in a
+    given dataframe
+
+    input
+    n: Number of top ingredients to retrieve
+    df: recipes dataframe 
+
+    output
+    top_ingr_df: dataframe containing the top n ingredients per cuisine
+    '''
+
+    grouped_recipes = df.groupby("cuisine").sum()
+    number_cuisines = grouped_recipes.shape[0]
+
+    ingr_dict = {}
+
+    for i in range(0, number_cuisines):
+
+        ocurrences = grouped_recipes.loc[i].sort_values(
+            ascending = False).reset_index().iloc[0:n]
+        ocurrences.columns = ["ingredient", "ocurrence"]
+        top = ocurrences["ingredient"].to_list()
+        ingr_dict[f"{i}"] = top
+
+    top_ingr_df = pd.DataFrame(data = ingr_dict)
+
+    return top_ingr_df
+
+def specific_ingredients(df):
+    '''
+    This function retrieves the specific ingredients that are only used in a 
+    cuisine.
+
+    input: 
+    df: recipes dataframe
+
+    output:
+    specific_df: dataframe containing the specific ingredients used in a cuisine
+    '''
+    grouped_recipes = df.groupby("cuisine").sum().reset_index()
+    recipes_feats = df.drop(columns = "cuisine")
+
+    ocur = []
+    spec_ingr = []
+    cuisine = []
+
+    for i in recipes_feats.columns:
+        a = grouped_recipes[["cuisine", i]].where(grouped_recipes[i] >0).dropna()
+
+        if a.shape[0] == 1:
+            cuisine.append(a["cuisine"].iloc[0])
+            ocur.append(a[i].iloc[0])
+            spec_ingr.append(i)
+    
+    specific_df = pd.DataFrame(data = zip(spec_ingr, cuisine, ocur), 
+    columns = ["ingredient", "cuisine", "occurrence"])
+
+    return specific_df
+
+def countries():
+    '''
+    This function creates a dataframe with the country and its cuisine names
+
+    input:
+    None
+
+    output:
+    countries_df: Pandas dataframe containing the country and cuisine names
+    '''
+
+    countries = ['UK', 'Greece', 'Thailand', 'Italy', 'India', 'Spain', 
+    'Germany', 'Morocco', 'Mexico', 'Japan', 'France', 'China']
+    countries_adjective = ['English', 'Greek', 'Thai', 'Italian', 'Indian', 
+    'Spanish', 'German', 'Moroccan', 'Mexican', 'Japanese', 'French', 'Chinese']
+
+    countries_df = pd.DataFrame(data= zip(countries, countries_adjective), 
+    columns = ["country", "cuisine_name"])
+
+    return countries_df
+
 def mallet():
 
     pass
+
 
